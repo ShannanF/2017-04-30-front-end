ee8619b0add65cf539e57b5a3a349d0a9b159da3
ShannanF <shannanfeng@gmail.com>
2017-04-30 15:26:01 -0700

    implement login function

--- js/script.js
+++ js/script.js
@@ -32,5 +32,29 @@ $(document).ready(function() {
             alert("please provide a username and password for signup!");
         }
     });
+    $("#loginBtn").click(function(event) {
+    	event.preventDefault()
+    	var username = $("#username").val();
+    	var password = $('#password').val();
+    	if (username && password) {
+    		$.post("http://open-commerce.herokuapp.com/api/login", {
+    			username: username,
+    			password: password
+    		},
+    		function(response) {
+    			if (response.success) {
+    				var cookie = "x-access-token=" + response.token;
+    				document.cookie = cookie; 
+    				window.location.href = "/index.html";
+
+    			} else {
+    				alert(response.message);
+    			}
+    		});
+
+    	} else {
+    		alert("please provide a username and password for login!");
+    	} 
+    });
 
 });
